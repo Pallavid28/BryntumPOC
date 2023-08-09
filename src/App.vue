3dd196fbbbeeca271a6f08de49973fc3e3b75345
Pavlo Miklashevych <pavlomiklashevych@cloud.upwork.com>
2023-08-09 10:03:24 +0300

    Html renderer for ResourceTimeRanges

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="container">
         <!-- BryntumDemoHeader component is used for Bryntum example styling only and can be removed -->
         <bryntum-scheduler-pro
+            ref="scheduler"
             v-bind="schedulerConfig"
         />
     </div>
@@ -11,7 +12,44 @@
 <script>
 
 import { BryntumSchedulerPro  } from '@bryntum/schedulerpro-vue';
+import { ResourceTimeRanges, Override } from '@bryntum/schedulerpro';
 import { schedulerConfig } from '@/AppConfig2';
+
+// Use it until https://github.com/bryntum/support/issues/5839 is fixed
+class ResourceTimeRangesOverride {
+  static get target() {
+    return {
+      class: ResourceTimeRanges,
+      product: 'schedulerpro',
+    }
+  }
+
+  // Provides renderer for ResourceTimeRanges
+  onEventDataGenerated(renderData) {
+    const { eventRecord } = renderData;
+
+    // Calls node_modules/@bryntum/schedulerpro/source/lib/Scheduler/feature/base/ResourceTimeRangesBase.js::onEventDataGenerated
+    this._overridden.onEventDataGenerated.call(this, renderData);
+
+    // Calls node_modules/@bryntum/schedulerpro/source/lib/Scheduler/feature/ResourceTimeRanges.js::shouldInclude
+    // If true, eventRecord is a ResourceTimeRange
+    if (this.shouldInclude(eventRecord)) {
+      // Remove text
+      delete renderData.eventContent.text;
+
+      // Add html
+      renderData.eventContent.html = `
+        <div class="reservation">
+          <div class="reservation-amount">${eventRecord.reservationAmount}</div>
+          <div class="reservation-price">${eventRecord.name}</div>
+          <div class="reservation-minNight">${eventRecord.minNight}</div>
+        </div>
+      `;
+    }
+  }
+}
+
+Override.apply(ResourceTimeRangesOverride);
 
 export default {
     name : 'App',
@@ -23,6 +61,32 @@ export default {
     data() {
         return { schedulerConfig };
     },
+
+  mounted() {
+    const scheduler = this.$refs.scheduler.instance;
+
+    scheduler.on({
+      resourceTimeRangeClick: this.onResourceTimeRangeClick,
+      thisObj : scheduler
+    });
+  },
+  methods: {
+    /**
+     * source : Scheduler
+     * feature : ResourceTimeRanges
+     * resourceTimeRangeRecord : ResourceTimeRangeModel
+     * resourceRecord : ResourceModel
+     * domEvent
+     */
+    // https://bryntum.com/products/schedulerpro/docs/api/Scheduler/view/SchedulerBase#event-resourceTimeRangeClick
+    onResourceTimeRangeClick({ source, feature, resourceTimeRangeRecord, resourceRecord, domEvent }) {
+      console.log('onResourceTimeRangeClick', source, feature, resourceTimeRangeRecord, resourceRecord, domEvent);
+
+      if (domEvent.target.closest('.reservation-amount')) {
+        alert(`Reservation amount is ${resourceTimeRangeRecord.reservationAmount}`)
+      }
+    }
+  }
 
 };
 </script>
